e077695c11eecee7fbbf9c83d30d333b1a8e89cf
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-08-09 20:36:47 -0300

    Callback, Promise, async e await adicionado

--- aula 01/index.js
+++ aula 01/index.js
@@ -1,1 +1,59 @@
-console.log('Hello World!!!')
+console.log('Exemplo de callback')
+
+setTimeout(
+    function () {
+        console.log('Executando tarega!!!')
+    },
+    1000
+)
+
+setTimeout(() => console.log('Executando tarega 2.0!!!'), 2000)
+
+const myMethod = () => console.log('Executando tarefa 3.0!!!')
+
+myMethod()
+
+setTimeout(myMethod, 2000)
+
+console.log('print depois do setTimeout')
+
+console.log('Exemplo de Promisse')
+
+const myResolovedPromise = () => new Promise((resolved, rejected) => {
+    resolved('Deu certo resolver a promise')
+})
+
+myResolovedPromisse()
+    .then(data => console.log(data))
+
+const myRejectPromise = () => new Promise((resolved, rejected) => {
+    rejected('Deu certo rejeitar a promise')
+})
+
+myRejectedPromisse()
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+const now = (new Date()).getMinutes()
+console.log(now)
+
+const myPromise = () => new Promise((resolved, rejected) => {
+    if (now % 2 == 0) {
+        resolved('Estou em um número PAR')
+    } else {
+        rejected('Estou em um número ÍMPAR')
+    }
+})
+
+myPromise()
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+console.log('Exemplo de Async-Await')
+
+const mySecondMethod = () => {
+    console.log('Estou no segundo metodo')
+    console.log('Estou saindo do segundo metodo')
+}
+
+mySecondMethod()
