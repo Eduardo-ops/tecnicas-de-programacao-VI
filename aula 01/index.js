6646c7657f8ef49db2e180e3ced6719da8b17990
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-08-16 16:39:41 -0300

    Callback, promise e async/await implementados com estudos externos em artigos.

--- aula 01/index.js
+++ aula 01/index.js
@@ -1,8 +1,17 @@
 console.log('Exemplo de callback')
 
+/* 
+Um callback é umM Listener, quando uma função acabar será disparada quando e se um evento acontecer.
+Foi inventado para rodar no front-end dos sites desde o princício.
+Com a criação de um código para ser disparado no clique de um botão, ou seja, no futuro.
+Nâo sendo na sequência linear do programa, e sim, para ser assíncrono, fora da sincronia normal de um código 
+*/
+
+/* Um callback, é uma função para ser chamada de volta quando outra função terminar. */
 setTimeout(
     function () {
         console.log('Executando tarega!!!')
+        // Código
     },
     1000
 )
@@ -19,6 +28,41 @@ console.log('print depois do setTimeout')
 
 console.log('Exemplo de Promisse')
 
+/* 
+Promises ainda é a base e deve ser entendida, é um objeto usado para processamento assíncrono
+uma vez que a promise é chamada, ela inicia-se um peding state(pendente), com isso, 
+a função caller(que chamou a promise) continue sendo executada enquanto espera pela promise terminar
+seu próprio processamento e retonar ao caller algum feedback. Quando esse retorno acontece, a promise 
+é retornada em resolved state ou rejected state.
+*/
+
+/* Modelo do estudo realizado por conta própria */
+function soAceitaPares() {
+    const promise = new Promise((resolve, reject) => {
+        /* Chamando resolve quando a execução for bem sucedida. */
+        if (numero % 2 == 0) {
+            const resultado = 'O número é par!'
+            resolve(resultado);
+        }
+        /* Chamando reject quando a execução for malsucedido sucedida. */
+        else {
+            reject(new Error('Você passou um número ímpar!'))
+        }
+    })
+}
+
+/* Chamando a promise e pegando e capturando seu retorno posiitvo ou negativo. */
+soAceitaPares(2)
+    .then(result => console.log('Promise resolved:' + result))
+    .catch(Error => console.log('Promise rejected:' + error));
+
+/*
+ O teste é imprimido primeiro, por que parece síncrono, mas a função é chamada e o código continua executando,
+imprimi o teste primeiro e só quando a função temrinar é que vai executar o then ou o catch. 
+*/
+console.log('teste')
+
+/* Modelo do professor */
 const myResolovedPromise = () => new Promise((resolved, rejected) => {
     resolved('Deu certo resolver a promise')
 })
@@ -50,6 +94,17 @@ myPromise()
     .catch(error => console.log(error))
 
 console.log('Exemplo de Async-Await')
+
+/* 
+Função declarada como async, significa que o valor de retorno da função será uma promise, se a promise
+se resolver normalmente, o objeto - promise retornaráo valor.Caso lance uma exceção, podemos usar o try/catch
+como estamos acostumados em programas síncronos. 
+*/
+async function getUser(userId) {
+    let resonse = await fetch('https://api.com/user/$userId')
+    let userData = await Response.json()
+    return userData.name
+}
 
 const myFuturePromise = () => {
     return new Promise((resolved, rejected) => {
